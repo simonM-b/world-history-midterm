3d0ca09a79e165d0570743c9f80a965012d09a53
simonM-b <slivwrm88@gmail.com>
2026-01-15 16:03:06 -0500

    camera controlls

---
+++ GLOBAL.gd
@@ -0,0 +1,18 @@
+extends Node
+
+const CAMERA_Y = -550
+const CAMERA_X_OFFSET = 300
+
+var playerPos
+var playerVel
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ scene + script/staticYFollowPlayer.gd
@@ -0,0 +1,13 @@
+extends Camera2D
+
+const SMALLER_X_LIMIT = -500
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	limit_left = SMALLER_X_LIMIT
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.y = GLOBAL.CAMERA_Y
+	position.x = GLOBAL.playerPos.x + GLOBAL.CAMERA_X_OFFSET*sign(GLOBAL.playerVel.x)#depending on what direction the player is it offsets the camera infront of the player #sign is -1 if input is below zero. 1 if above zero and 0 is is zero
