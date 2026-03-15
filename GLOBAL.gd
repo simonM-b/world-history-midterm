11f1e245fe053aedba205610830053eb647905de
Simon Mickal <slivwrm88@gmail.com>
2026-03-15 16:56:44 -0400

    added some things from end to global

--- GLOBAL.gd
+++ GLOBAL.gd
@@ -42,6 +42,13 @@ var shopCostItem = {
 
 var listOfMapPeopleMaps = []
 
+var ItemsLeftToEnd = { #the amount of items needed to end the game in the last pannel
+	cotton=0,
+	mach=0,
+	ppl=0,
+	boxes=0
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
